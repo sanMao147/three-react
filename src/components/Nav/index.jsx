8da7b9cae6f40d090172a382d90099e70b8a6b47
wohool <1529063213@qq.com>
2023-03-18 11:46:19 +0800

    fix

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,47 +1,61 @@
 import './index.scss'
 import { Button, Drawer } from 'antd'
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 const Index = () => {
   const [open, setOpen] = useState(false)
-  const router = useNavigate()
+  const [visible, setVisible] = useState(false)
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+  useEffect(() => {
+    if (pathname !== '/home') {
+      setVisible(true)
+    } else {
+      setVisible(false)
+    }
+  }, [pathname])
+  const handleRoute = e => {
+    switch (e) {
+      case 'sea':
+        navigate('/home/ocean')
+        break
 
-  const handleRoute = () => {
-    router('/home/ocean')
+      default:
+        navigate('/home')
+        break
+    }
+    setOpen(false)
   }
-  const showDrawer = () => {
-    setOpen(true);
-  };
 
-  const onClose = () => {
-    setOpen(false);
-  };
-  const drawbody = {
-    backgroundColor: '#eee',
-    color: '#000'
-  }
-  const drawhead = {
-    backgroundColor: '#ccc'
-  }
   return (
     <>
       <Button
         className="nav"
         type="primary"
-        onClick={showDrawer}
+        onClick={() => {
+          setOpen(true)
+        }}
       >
         导航
       </Button>
       <Drawer
-        bodyStyle={drawbody}
-        headerStyle={drawhead}
+        bodyStyle={{
+          backgroundColor: '#eee',
+          color: '#000'
+        }}
+        headerStyle={{
+          backgroundColor: '#ccc'
+        }}
         title="页面菜单"
         placement="left"
-        onClose={onClose}
+        onClose={() => {
+          setOpen(false)
+        }}
         open={open}
       >
-        <p onClick={handleRoute}>海岛风情</p>
+        {visible ? <p onClick={() => handleRoute('home')}>返回首页</p> : ''}
+        <p onClick={() => handleRoute('sea')}>海岛风情</p>
         <p>Some contents...</p>
         <p>Some contents...</p>
       </Drawer>
