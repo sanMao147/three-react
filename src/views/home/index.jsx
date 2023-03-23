89f7da27f3d1c8a4d10f370b4c918793e39709da
wohool <1529063213@qq.com>
2023-03-23 11:21:24 +0800

    fix

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,7 +1,8 @@
 import Dashboard from '@/components/Dashboard'
 import Nav from '@/components/Nav'
+import router from '@/router'
 import React, { useState } from 'react'
-import { useNavigate, Outlet, useLocation } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 const Index = () => {
   const { pathname } = useLocation()
@@ -9,7 +10,7 @@ const Index = () => {
   return (
     <>
       <Nav />
-      {pathname == '/home' ? <Dashboard /> : ''}
+      {pathname === router.routes[1].path ? <Dashboard /> : ''}
 
       <Outlet></Outlet>
     </>
